f38ad9bfc9272a221cd52a6716f325ba20221db2
Robert Rees <robert@wegotpop.com>
2019-07-16 23:24:55 +0100

    Creates a basic rendering function with a snake state object

--- js/snake.js
+++ js/snake.js
@@ -1,6 +1,9 @@
 
 const size = 500;
 
+const backgroundColour = '#ffffff';
+const foregroundColour = '#000000';
+
 const canvas = document.getElementById("bg");
 
 const ctx = canvas.getContext('2d');
@@ -9,3 +12,37 @@ ctx.font = '50px serif';
 ctx.fillText("Hello world", 0,50);
 
 ctx.strokeRect(0, 0, size, size)
+
+const snake = [];
+
+snake.push({x: canvas.width/2, y: canvas.height/2});
+
+function handleKeyPress(event) {
+	console.log(event);
+}
+
+function drawBox(context, x, y, width, height, colour) {
+	context.beginPath();
+	context.rect(x,y,width,height);
+	context.fillStyle = colour;
+	context.fill();
+	context.closePath();
+}
+
+function render() {
+	snake.forEach(snakePart => {
+		console.log(snakePart);
+		drawBox(
+			ctx,
+			snakePart.x,
+			snakePart.y,
+			10,
+			10,
+			foregroundColour,
+			);
+	});
+}
+
+render();
+
+window.addEventListener('keyup', handleKeyPress, false);
